e7b5c189eaf34ad1ce99249ce0f03d53e20072ea
Anderson Santos <disantidev@gmail.com>
2025-10-24 02:29:28 -0300

    fix: shell message after input

--- src/main.c
+++ src/main.c
@@ -6,17 +6,24 @@
 
 int main(int argc, char *argv[]) {
     char* input_line = NULL;
-
+    
     if ((input_line = (char*)malloc(INPUT_LINE_SIZE * sizeof(char))) == NULL) {
         fprintf(stderr, "Error: Unable to allocate memory for input line.\n");
         return EXIT_FAILURE;
     }
 
     printf("Welcome to My Own Shell!\n\n");
-    printf("shell> ");
 
-    while (fgets(input_line, INPUT_LINE_SIZE, stdin) != NULL) {
+    while (1) {
+        printf("shell> ");
+
+        if(fgets(input_line, INPUT_LINE_SIZE, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
         input_line[strlen(input_line) - 1] = '\0';
+
         printf("shell> You entered \"%s\"\n", input_line);
     }
 
